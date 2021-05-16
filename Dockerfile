9413cb540bf47bff5cc44824131335120d7730da
Thiago Finardi <tfinardi@gmail.com>
2021-05-15 22:39:45 -0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.7
+WORKDIR /app
+COPY app /app
+COPY conf /app/conf
+RUN pip install gunicorn
+RUN pip install -r requirements.txt
+ENV PORT=8080
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 main:app
+
---
+++ app/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+
+def readJson():
+    with open('./conf/config.json') as config:
+        return json.load(config)
+
+
+@app.route("/")
+def main():
+    texts = readJson()
+
+    return render_template('index.html', texts=texts)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
